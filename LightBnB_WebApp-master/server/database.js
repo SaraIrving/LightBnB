1ae57324018f86fcc38ee5c9716f9ff804827b18
Sara Irving <sarakirving@gmail.com>
2020-07-23 23:59:46 +0000

    Refactored getAllProperties to use a LEFT JOIN instead of a JOIN so it includes properties with no reviews. Refactored addProperty function to insert data gathered from user and create a new row in the properties table of the database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -54,7 +54,8 @@ const addUser =  function(user) {
     VALUES ($1, $2, $3)
     RETURNING*`, [user.name, user.email, user.password]
   )
-  .then(res => res.rows[0]);
+  .then(res => res.rows[0])
+  .catch('There was a problem while attempting to add this user!');
 }
 exports.addUser = addUser;
 
@@ -76,6 +77,7 @@ const getAllReservations = function(guest_id, limit = 10) {
     ORDER BY start_date 
     LIMIT $2`, [guest_id, limit])
     .then(res => res.rows)
+    .catch('We can\'t find any past reservations!');
 }
 exports.getAllReservations = getAllReservations;
 
@@ -95,7 +97,7 @@ const getAllProperties = function(options, limit = 10) {
    let queryString = `
    SELECT properties.*, avg(property_reviews.rating) as average_rating
    FROM properties
-   JOIN property_reviews ON properties.id = property_id
+   LEFT JOIN property_reviews ON properties.id = property_id
    `;
  
    // 3
@@ -169,7 +171,8 @@ const getAllProperties = function(options, limit = 10) {
  
    // 6
    return pool.query(queryString, queryParams)
-   .then(res => res.rows);
+   .then(res => res.rows)
+   .catch('We can\'t find any properties!');
 }
 exports.getAllProperties = getAllProperties;
 
@@ -180,9 +183,16 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
+  return pool.query(
+    `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *`, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms]
+  )
+  .then(res => res.rows[0])
+  .catch(console.log('Something has gone wrong when we tried to add your property. Sorry!'));
 }
 exports.addProperty = addProperty;
