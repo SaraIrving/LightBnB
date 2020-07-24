41aa2c6c2fe4270580ca86728935bdd27d6acd9f
Sara Irving <sarakirving@gmail.com>
2020-07-24 01:50:58 +0000

    Refactored addProperty so that it multiplies the property.cost_per_night property by 100 to convert it to cents to match the database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -190,7 +190,7 @@ const addProperty = function(property) {
   return pool.query(
     `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
     VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
-    RETURNING *`, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms]
+    RETURNING *`, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night * 100, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms]
   )
   .then(res => res.rows[0])
   .catch(console.log('Something has gone wrong when we tried to add your property. Sorry!'));
